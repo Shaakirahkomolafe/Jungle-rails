ef33f98a2b653d156f9ca74819378453241916cf
Shakirat komolafe <damolakomolafe.dk@gmail.com>
2021-09-22 17:13:15 -0400

    add test for add to cart function

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,10 +4,14 @@ class SessionsController < ApplicationController
   def create
     if user = User.authenticate_with_credentials(params[:email], params[:password])
       # success logic, log them in
+      session[:user_id] = user.id
+      redirect_to '/'
     else
       # failure, render login form
+      redirect_to '/login'
+
     end
-  end
+ end
 
   def destroy
     session[:user_id] = nil
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,16 +2,17 @@ class User < ActiveRecord::Base
   has_secure_password
   validates :name, presence: true
   validates :password, length: { minimum: 8 }, if: :password_digest_changed?
-  validate :validate_case_sensitive_emails
-  validates :email, :uniqueness => {:case_sensitive => false}
+  validates :email, presence:true, uniqueness: true
+   before_create {self.email = email.strip.downcase}
 
-  def validate_case_sensitive_emails
-    if(self.email =~ /[A-Z]/) === 0
-        errors.add(:email, "should not have capital letter")
-    end 
-  end
+  # def validate_case_sensitive_emails
+  #   self.email = self.email.strip.downcase
+  #   # if(self.email =~ /[A-Z]/) === 0
+  #   #     errors.add(:email, "should not have capital letter")
+  #   end 
+  # end
   def self.authenticate_with_credentials(email, password)
-    user = User.find_by_email(email.strip)
+    user = User.find_by_email(email.strip.downcase)
     # If the user exists AND the password entered is correct.
     if user && user.authenticate(password)
       return user
